0e4bb35afbecb42dbb2d6afa4a6f4949f225e7bc
Matteo Petri <135416861+makfrake@users.noreply.github.com>
2025-08-30 18:07:47 +0200

    Added first plot function for residual

--- Postprocessing.py
+++ Postprocessing.py
@@ -1,12 +1,23 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 
 data_folder = Path("C:/Users/matte/PycharmProjects/Fortran-Based-CFD-Solver/Fortran_Solver/")
 file_to_open = data_folder / "residuals.csv"
 
-x = pd.read_csv(file_to_open, sep=" , ", header=None, engine='python')
-print(x[0])
+res = pd.read_csv(file_to_open, sep=" , ", header=None, engine='python')
+res.columns = ['Energy','Mass','MomentumX','MomentumY']
+print(res['Energy'])
+
+x = np.linspace(0,len(res)-1,len(res))
+
+plt.figure(1)
+for i in range(0,len(res.columns)-1):
+    plt.plot(x, res[res.columns[i]])
+    plt.xlabel('x')
+    plt.show()
+
+
 
 
