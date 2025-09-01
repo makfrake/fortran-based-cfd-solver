a2a7e5d2c65810f2be4b3d2a6811c0d1bad95f5c
Matteo Petri <135416861+makfrake@users.noreply.github.com>
2025-09-01 13:59:27 +0200

    Uploaded project

--- src/main.f90
+++ src/main.f90
@@ -24,6 +24,8 @@ Program test2d
 
     time=0.
 
+    open(unit=90, file='residuals.txt')
+
 	do k=1,kfinal
 
 		call compute_dt
@@ -39,18 +41,21 @@ Program test2d
 			call compute_norm_residuals
 			call compute_norm_entropy
 			call save_wall_data
+            !write(90,*) '# iteration',',','Energy',',','Mass',',','MomentumX',',','MomentumY'
+
 
 		end if
 
 
 		if(mod(k,kout).eq.0)then
 			call write_tecplot_file(k,0.)
+			write(90,*) k,',',norm2_residuals(1),',',norm2_residuals(2),',',norm2_residuals(3),',',norm2_residuals(4)
 		end if
 
 
 	end do
 
-
+    close(90)
 
 End Program
 
